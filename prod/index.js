acc378f0a600fb25630c11da3343a73d49ddfad5
xiaolei <xiaolei@xiaoleideMacBook-Pro.local>
2022-03-29 09:13:40 +0800

    prod deploy

---
+++ prod/index.js
@@ -0,0 +1,14 @@
+const config = require('../../config')
+
+export default {
+  // 配置显示在浏览器标签的title
+  title: '',
+  // 域名
+  domain: 'https://artwormz.com',
+  // 基本URL：测试服务器API，打包发布
+  // baseUrl: 'https://testnet.artwormz.com/iobots',
+  // 基本URL：生产服务器API，打包发布
+  baseUrl: 'https://artwormz.com/iobots',
+  // 基本URL：本地API，本地开发
+  // baseUrl: 'http://' + config.dev.host + ':' +  config.dev.port + '/dev/iobots',
+}
